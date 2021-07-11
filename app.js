00db536f44e4682b5cd40abc3ba404f5cd7ebd4d
Ravshan <rkhudoyberdiev@gmail.com>
2021-07-12 03:34:11 +0500

    Getting dinosaurs

--- app.js
+++ app.js
@@ -10,19 +10,20 @@
         this.diet = diet;
     }
 
- 
-    
-    // Create an array of Dino Objects from fetched json file
-    let dinos = [];
+    // An array to hold javascript dinosaur objects
+    let dinosaurs = [];
+    // Push to the above array of Dino Objects from fetched json file
+    function getDinos() {
+        fetch('./dino.json')
+        .then(res => res.json())
+        .then(data => {
+            data.Dinos.forEach(dino => {
+               dinosaurs.push(new Dino(dino.species, dino.height, dino.weight, dino.diet, dino.when, dino.where, dino.facts));
+            });
+        })
+        .catch(err => console.error(err));
+    }
 
-    fetch('./dino.json')
-    .then(res => res.json())
-    .then(data => {
-        data.Dinos.forEach(dino => {
-           dinos.push(new Dino(dino.species, dino.height, dino.weight, dino.diet, dino.when, dino.where, dino.facts));
-        });
-    })
-    .catch(err => console.error(err));
 
 
 
@@ -52,15 +53,18 @@
 // On button click, prepare and display infographic
 document.querySelector('#dino-compare #btn').onclick = function() {
 
-    // Create Human Object
-    let human = {}
-    // Use IIFE to get and store human data from form
-    (function() {
+    // Create Human Object and use IIFE to get the human data from the form
+    let human = (function() {
+        let human = {};
         human.name = document.getElementById('name').value;
         human.height = document.getElementById('feet').value;
         human.weight = document.getElementById('weight').value;
         human.diet = document.getElementById('diet').value;
+        return human;
     })();
     console.log(human);
+    getDinos();
+    console.log(dinosaurs);
+
     hideForm();
 };
