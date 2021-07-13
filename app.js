56abfb6188f21dc9106974d32ecfa5ea91b1ee62
Ravshan <rkhudoyberdiev@gmail.com>
2021-07-13 17:40:02 +0500

    Major rewrite using class constructors

--- app.js
+++ app.js
@@ -4,6 +4,11 @@
     const mainGrid = document.getElementById('grid');
     const mainForm = document.getElementById('dino-compare');
     
+    // An array to hold javascript dinosaur objects
+    const dinosaurs = [];
+    // A variable to hold a human object
+    let human = '';
+
     // Dino Class Constructor
     class Dino {
         constructor(species, height, weight, diet, when, where, facts) {
@@ -17,71 +22,91 @@
         }
 
         // Compare Dino to Human Height Method
-        compareDinoHeight(humanHeight) {
-            let difference = this.height - humanHeight;
+        compareDinoHeight() {
+            console.log('insideclass');
+            let difference = this.height - human.height;
             let newFact = `${this.species} were ${Math.abs(difference)} inches ${difference>=0 ? 'higher' : 'smaller'} than you`;
             return newFact;
         }
 
         // Compare Dino to Human Weight Method
-        compareDinoWeight(humanWeight) {
-            let difference = this.weight - humanWeight;
+        compareDinoWeight() {
+            let difference = this.weight - human.weight;
             let newFact = `${this.species} were ${Math.abs(difference)} pounds ${difference>=0 ? 'heavier' : 'lighter'} than you`;
             return newFact;
         }
 
         // Compare Dino to Human Diet Method
-        compareDinoDiet(humanDiet) {
-            let sameDiet = this.diet.toLowerCase() == humanDiet;
+        compareDinoDiet() {
+            let sameDiet = this.diet.toLowerCase() == human.diet;
             let newFact = '';
             if (sameDiet) {
-                newFact = `${this.species} and you had the same diet. You both had a ${humanDiet} diet.`
+                newFact = `${this.species} and you had the same diet. You both had a ${human.diet} diet.`
             } else {
-                newFact = `${this.species} and you had a completely different diet. ${this.species} preferred to be ${this.diet} while you prefer the ${humanDiet} diet.`
+                newFact = `${this.species} and you had a completely different diet. ${this.species} preferred to be ${this.diet} while you prefer the ${human.diet} diet.`
             }
             return newFact;
         }
 
+        // Get profile pic URLs
         getProfilePic() {
-            return 'images' + this.species.toLowerCase() + ".png";
+            return 'images/' + this.species.toLowerCase() + ".png";
         }
 
-        addToFacts(humanWeight, humanHeight, humanDiet) {
-            if ((this.species.toLowerCase() != "pigeon") || (this.species.toLowerCase() != "human")) {
-                this.facts.push(this.compareDinoHeight(humanHeight));
-                this.facts.push(this.compareDinoWeight(humanWeight));
-                this.facts.push(this.compareDinoDiet(humanDiet));
+        // Add 3 more facts to dinos if not pigeon species
+        addToFacts() {
+            if (this.species.toLowerCase() != "pigeon") {
+                this.facts.push(this.compareDinoHeight(), this.compareDinoWeight(),this.compareDinoDiet());
             }
         }
 
+        // Choose a random fact if not human
+        randomFact() {
+            if (this.species.toLowerCase() != "human") {
+                return this.facts[Math.floor(Math.random() * this.facts.length)];
+            }
+        }
+
+        // Build a tile
         buildTile() {
-            const displayHumanTile = dino.species.toLowerCase() == 'human';
+            const displayHumanTile = this.species.toLowerCase() == 'human';
             let mainImage = '<img src="' + this.getProfilePic() +'" />';
-            let h3Title = '<h3>'+ (displayHumanTile ? dino.name : dino.species) + '</h3>';
-            let heightPara = '<p>Height:' + dino.height + '</p>';
-            let weightPara = '<p>Weight:' + dino.weight + '</p>';
-            let dietPara = '<p>Diet:' + dino.diet + '</p>';
-            return '<div class="grid-item">'+ mainImage + h3Title + heightPara + weightPara + dietPara + (!displayHumanTile ? '<div><p>' + dino.when + '</p><p>' + dino.where + '</p><p>' + dino.facts[Math.floor(Math.random() * dino.facts.length)] + '</p></div>' : '') + '</div>';
+            let h3Title = '<h3>'+ (displayHumanTile ? this.name : this.species) + '</h3>';
+            let heightPara = '<p>Height:' + this.height + '</p>';
+            let weightPara = '<p>Weight:' + this.weight + '</p>';
+            let dietPara = '<p>Diet:' + this.diet + '</p>';
+            return '<div class="grid-item">'+ h3Title + mainImage + heightPara + weightPara + dietPara + (!displayHumanTile ? '<div><p>' + this.when + '</p><p>' + this.where + '</p><p>' + this.randomFact() + '</p></div>' : '') + '</div>';
         }
     }
-    
 
-    // An array to hold javascript dinosaur objects
-    const dinosaurs = [];
+    // Human Constructor Class
+    class Human extends Dino {
+        constructor(name, height, weight, diet, species) {
+            super(species, height, weight, diet);
+            this.name = name;
+        }
+    }
+
     // Get dino.json file contents and save them to an array of objects
-    const jsonResponse = await fetch('./dino.json'); 
-    const dinoDataFromJson = await jsonResponse.json(); 
-    // Creating javascript objects out of fetched json objects
-    dinos.Dinos.forEach((dino) => {
-        // Saving an object to the global dinosaurs variable
-        dinosaurs.push(new Dino(dino.species, dino.height, dino.weight, dino.diet, dino.when, dino.where, dino.facts)); 
-    });
+    (async function createDinoObjects() {
+        const jsonResponse = await fetch('./dino.json'); 
+        const dinoDataFromJson = await jsonResponse.json(); 
+        // Creating javascript objects out of fetched json objects
+        dinoDataFromJson.Dinos.forEach((dino) => {
+            // Saving an object to the global dinosaurs variable
+            dinosaurs.push(new Dino(dino.species, dino.height, dino.weight, dino.diet, dino.when, dino.where, dino.facts)); 
+        });
+    })();
+
 
     
     // Generate ready html grid tiles from dino objects
     function getHtml() {
         let htmlContent = '';
         dinosaurs.forEach(dino => {
+            // Add 3 more facts to the array
+            console.log('getht');
+            (dino.species.toLowerCase() != "human") ? dino.addToFacts() : '';
             htmlContent += dino.buildTile();
         });
         return htmlContent;
@@ -96,24 +121,20 @@
 submitButton.onclick = function() {
 
     // Create Human Object and use IIFE to get the human data from the form
-    let human = (function() {
-        let humanObj = {};
-        humanObj.name = document.getElementById('name').value;
-        humanObj.height = document.getElementById('feet').value*12+document.getElementById('inches').value;
-        humanObj.weight = document.getElementById('weight').value;
-        humanObj.diet = document.getElementById('diet').value.toLowerCase();;
-        humanObj.species = 'Human';
-        return humanObj;
+    human = (function() {
+        const name = document.getElementById('name').value;
+        const height = document.getElementById('feet').value*12+document.getElementById('inches').value;
+        const weight = document.getElementById('weight').value;
+        const diet = document.getElementById('diet').value.toLowerCase();;
+        const species = 'Human';
+        return new Human(name, height, weight, diet, species);
     })();
-    // after we create human add comparison methods and then facts and splice
-            // Add human object into dinosaurs array
-            // dinosaurs.splice(4,0,human);
-    // Get the prepared array with javascript objects including human object and more facts inside each object
-    getDinoObjectsFromJSON(human);
+    console.log(human);
+    // Add human object into dinosaurs array
+    dinosaurs.splice(4,0,human);
 
     // Add tiles to DOM
     mainGrid.innerHTML = getHtml();
-    
     
 
     // hideForm();
